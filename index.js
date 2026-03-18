71db16ad7f0395deffce89a2b138336238f039e7
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-18 17:28:15 +0530

    counter done

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client"
+
+function App(){
+    const [count,setCount]=useState(0);
+    const [finnum,setFib]=useState();
+    return (
+        <div className="parent">
+            <div>
+                <h1>{count}</h1>
+                <button onClick={()=>setCount(count+1)}>Increase</button>
+                <button onClick={()=>setCount(count-1)}>Decrease</button>
+            </div>
+            
+
+        </div>
+    )
+
+}
+const reactRoot=ReactDOM.createRoot(document.getElementById('root'));
+reactRoot.render(<App/>);
